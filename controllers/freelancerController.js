a53fcb3d39d29c487f1085d4c95bd3388eaa5713
Rinkoo-ctrl <rinkookandpal2000@gmail.com>
2025-04-06 21:31:53 +0530

    Implement authentication middle to create freelancer api and fix the code

--- controllers/freelancerController.js
+++ controllers/freelancerController.js
@@ -1,8 +1,9 @@
 const Freelancer = require('../models/Freelancer');
 
 exports.createOrUpdateFreelancer = async (req, res) => {
-    const { userId, name, bio, skills, location, images } = req.body;
+    const { name, bio, skills, location, images } = req.body;
     try {
+        const userId = req.user.id;
         const profile = await Freelancer.findOneAndUpdate(
             { userId },
             { name, bio, skills, location, images },
