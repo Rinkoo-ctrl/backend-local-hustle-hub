32b67f3df92105bc5f7ebc4e1eb148b808a0b11f
Rinkoo-ctrl <rinkookandpal2000@gmail.com>
2025-04-06 20:45:18 +0530

    Fix file name for service route

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const serviceController = require('../controllers/serviceController');
+const serviceController = require('../controllers/serviceController.js');
 
 router.post('/', serviceController.createService);
 router.get('/', serviceController.getAllServices);
--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use(cors());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use('/api/freelancers', require('./routes/freelancerRoutes'));
-app.use('/api/services', require('./routes/servicesRoutes'));
+app.use('/api/services', require('./routes/serviceRoutes.js'));
 
 app.listen(5000, () => console.log("Server running on port 5000"));
