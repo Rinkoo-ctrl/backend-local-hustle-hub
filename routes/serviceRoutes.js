93b90120c962c5a9de663078d435a2235810acd6
Rinkoo-ctrl <rinkookandpal2000@gmail.com>
2025-04-07 23:52:26 +0530

    Add authentication at create service api

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const serviceController = require('../controllers/serviceController.js');
+const authenticateToken = require('../middleware/authMiddleware');
 
-router.post('/', serviceController.createService);
+router.post('/', authenticateToken, serviceController.createService);
 router.get('/', serviceController.getAllServices);
 router.put('/:id', serviceController.updateService);
 router.delete('/:id', serviceController.deleteService);
