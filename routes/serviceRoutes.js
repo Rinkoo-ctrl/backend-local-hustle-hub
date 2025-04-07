88283bed912e4f7f46ccccc6077995f751e79262
Rinkoo-ctrl <rinkookandpal2000@gmail.com>
2025-04-07 23:54:25 +0530

    Add authentication at get service api

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -4,7 +4,7 @@ const serviceController = require('../controllers/serviceController.js');
 const authenticateToken = require('../middleware/authMiddleware');
 
 router.post('/', authenticateToken, serviceController.createService);
-router.get('/', serviceController.getAllServices);
+router.get('/', authenticateToken, serviceController.getAllServices);
 router.put('/:id', serviceController.updateService);
 router.delete('/:id', serviceController.deleteService);
 
