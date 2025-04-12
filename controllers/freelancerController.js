4f3ba852c3d5db6416d05e7b8c7453d2ca4e7fba
Rinkoo-ctrl <rinkookandpal2000@gmail.com>
2025-04-12 20:09:50 +0530

    Modify freelancer schema and createOrUpdateFreelancer api to save freelancer profile data

--- controllers/freelancerController.js
+++ controllers/freelancerController.js
@@ -1,20 +1,40 @@
 const Freelancer = require('../models/Freelancer');
 
 exports.createOrUpdateFreelancer = async (req, res) => {
-    const { name, bio, skills, location, images } = req.body;
-    
+    const { name, bio, skills, locations } = req.body;
+
     try {
         const userId = req.user.id;
+        const location = locations[0];
+
+        if (!location || !location.coordinates || location.coordinates.length !== 2) {
+            return res.status(400).json({ message: "Invalid location format." });
+        }
+
+        const address = location.address;
+        const longitude = location.coordinates[0];
+        const latitude = location.coordinates[1];
+
         const profile = await Freelancer.findOneAndUpdate(
             { userId },
-            { name, bio, skills, location, images },
+            {
+                name,
+                bio,
+                skills,
+                address,
+                latitude,
+                longitude
+            },
             { new: true, upsert: true }
         );
+
         res.json(profile);
     } catch (err) {
+        console.error(err);
         res.status(500).json({ message: 'Error saving profile', error: err });
     }
 };
+
 
 exports.getFreelancerById = async (req, res) => {
     try {
--- models/Freelancer.js
+++ models/Freelancer.js
@@ -1,30 +1,18 @@
 const mongoose = require('mongoose');
 
-const FreelancerSchema = new mongoose.Schema({
+const freelancerSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     name: String,
     bio: String,
     skills: [String],
-    image: String,
-    locations: [
-        {
-            type: {
-                type: String,
-                enum: ["Point"],
-                default: "Point"
-            },
-            coordinates: {
-                type: [Number], // [longitude, latitude]
-                required: true
-            },
-            address: {
-                type: String,
-                required: true
-            }
-        }
-    ]
+    address: { type: String, required: true },
+    latitude: {
+        type: Number, required: true
+    },
+    longitude: {
+        type: Number, required: true
+    }
 });
 
-FreelancerSchema.index({ "locations": "2dsphere" });
 
-module.exports = mongoose.model('Freelancer', FreelancerSchema);
+module.exports = mongoose.model('Freelancer', freelancerSchema);
