76dddb50d5ba942ed9733301fda367a7ad7aa5d9
Rinkoo-ctrl <rinkookandpal2000@gmail.com>
2025-04-13 00:22:44 +0530

    Remove name from register api

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
     try {
-        const { name, email, password, role } = req.body;
+        const { email, password, role } = req.body;
 
         let user = await User.findOne({ email });
         if (user) return res.status(400).json({ message: "User already exists" });
@@ -13,7 +13,7 @@ exports.register = async (req, res) => {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        user = new User({ name, email, password: hashedPassword, role });
+        user = new User({ email, password: hashedPassword, role });
         await user.save();
 
         return res.status(201).json({ message: "User registered successfully. Please login." });
@@ -28,7 +28,7 @@ exports.login = async (req, res) => {
         const { email, password } = req.body;
 
         const user = await User.findOne({ email });
-        
+
         if (!user) return res.status(400).json({ message: "Invalid credentials!!!" });
 
         const isMatch = await bcrypt.compare(password, user.password);
@@ -37,10 +37,10 @@ exports.login = async (req, res) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 
         const freelancerProfile = await Freelancer.findOne({ userId: user._id });
-      
+
         res.json({
             token, user: {
-                id: user._id, name: user.name, email, isFreelancer: !!freelancerProfile, role: user.role,
+                id: user._id, email, isFreelancer: !!freelancerProfile, role: user.role,
                 freelancerProfile: freelancerProfile || null
             }
         });
